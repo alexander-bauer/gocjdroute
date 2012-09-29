4c9ce8c9a2ae0313c684b936bcabae77a128d369
SashaCrofter <sasha@crofter.org>
2012-09-29 10:05:01 -0400

    added some skeleton code with broken type conversion

--- pass.go
+++ pass.go
@@ -1,19 +1,22 @@
 package main
 
 import (
-	"math/rand"
-	"time"
+	"crypto/rand"
+	"math/big"
+	"encoding/binary"
 )
 
 //
 func GenPass(tag string) string {
-	rand.Seed(time.Now().UnixNano()) //"randomize" the seed
-
+	upperlim := big.NewInt(int64(0x59))
+	lowerlim := uint16(0x21)
+	
 	p := ""           //initialize an empty string for the password
 	for len(p) < 16 { //getting 128 bits
-		c := rand.Intn(0x59) //7 bit integers, 0x21-0x79 (inclusive)
-		c += 0x21            //we need ascii characters
-		p += string(c)
+		c, _ := rand.Int(rand.Reader, upperlim) //7 bit integers, 0x21-0x79 (inclusive)
+		v := binary.BigEndian.PutUint64(c)
+		v += lowerlim //move in to the ascii range
+		p += string(v)
 	}
 	password := tag + "_" + p
 	return password
