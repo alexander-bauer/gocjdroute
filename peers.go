cfeeb3b26b863aeb5ef7ccf2bc290eef0823dfe2
SashaCrofter <sasha@crofter.org>
2012-10-02 00:12:01 -0400

    minor documentation change

--- peers.go
+++ peers.go
@@ -4,7 +4,7 @@ import (
 	"cjdngo"
 )
 
-//Authorize adds a new entry to the authorizedPasswords block, generating a cryptographically strong password and returning it. Arguments 'name,' 'location,' and 'ipv6' are optional, but recommended for identifying the authorized node. It is highly recommended that every node be given a unique password.
+//Authorize adds a new entry to the authorizedPasswords block in 'conf,' generating a cryptographically strong password and returning it. Arguments 'name,' 'location,' and 'ipv6' are optional, but recommended for identifying the authorized node. It is highly recommended that every node be given a unique password.
 func Authorize(conf *cjdngo.Conf, name string, location string, ipv6 string) string {
 	p := GenPass(name)
 	newAuth := &cjdngo.AuthPass{name, location, ipv6, p}
