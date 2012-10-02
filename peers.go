3b322a452b27fcabe2ee23eee0c65129690c03f8
Sasha Crofter <sasha@crofter.org>
2012-10-02 12:45:14 -0400

    formatted and made repetetive-argument stylechange

--- peers.go
+++ peers.go
@@ -5,7 +5,7 @@ import (
 )
 
 //Authorize adds a new entry to the authorizedPasswords block in 'conf,' generating a cryptographically strong password and returning it. Arguments 'name,' 'location,' and 'ipv6' are optional, but recommended for identifying the authorized node. It is highly recommended that every node be given a unique password.
-func Authorize(conf *cjdngo.Conf, name string, location string, ipv6 string) string {
+func Authorize(conf *cjdngo.Conf, name, location, ipv6 string) string {
 	p := GenPass(name)
 	newAuth := &cjdngo.AuthPass{name, location, ipv6, p}
 	conf.AuthorizedPasswords = append(conf.AuthorizedPasswords, *newAuth)
@@ -14,11 +14,11 @@ func Authorize(conf *cjdngo.Conf, name string, location string, ipv6 string) str
 
 //ConnectTo adds an entry to the "connectTo" block (under 'interfaces.UDPInterface') with the given details. The arguments 'connection,' 'password,' and 'publicKey' are required, but 'name,' 'location,' and 'ipv6' are optional. They are recommended for identifying the target of the connection.
 //This will exit without making changes to 'conf' if any of the required arguments are not provided.
-func ConnectTo(conf *cjdngo.Conf, connection string, password string, publicKey string, name string, location string, ipv6 string) {
+func ConnectTo(conf *cjdngo.Conf, connection, password, publicKey, name, location, ipv6 string) {
 	if connection == "" || password == "" || publicKey == "" {
 		return //if the required fields are not filled out, exit
 	}
-	
+
 	conn := &cjdngo.Connection{name, location, ipv6, password, publicKey}
 	//creating the connnection
 	conf.Interfaces.UDPInterface.ConnectTo[connection] = *conn
@@ -33,7 +33,7 @@ func ConnectToMap(conf *cjdngo.Conf, connections map[string]*cjdngo.Connection) 
 	for i, j := range connections { //for every item in connections,
 		//i will be the connection information, such as ipv4 and port.
 		//j will be the Connection interface itself.
-		
+
 		//This will map the new Connection interface to the connection information on the existing Conf object.
 		conf.Interfaces.UDPInterface.ConnectTo[i] = *j
 	}
--- search.go
+++ search.go
@@ -9,7 +9,7 @@ import (
 //Returns the indexes of every AuthPass with at least one partial match in any of the above fields.
 func SearchAuth(conf cjdngo.Conf, term string) []int {
 	matches := make([]int, 0, len(conf.AuthorizedPasswords))
-	
+
 	for i := 0; i < len(conf.AuthorizedPasswords); i++ {
 		//check if the Name field contains it...
 		if strings.Contains(conf.AuthorizedPasswords[i].Name, term) {
