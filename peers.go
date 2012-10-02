6f4a6c4731d8ead9ca8a49858b9af2d79ebe566c
SashaCrofter <sasha@crofter.org>
2012-10-02 09:40:05 -0400

    added ConnectTo() for adding target connections based on string arguments

--- peers.go
+++ peers.go
@@ -11,3 +11,22 @@ func Authorize(conf *cjdngo.Conf, name string, location string, ipv6 string) str
 	conf.AuthorizedPasswords = append(conf.AuthorizedPasswords, *newAuth)
 	return p
 }
+
+//ConnectTo adds an entry to the "connectTo" block (under 'interfaces.UDPInterface') with the given details. The arguments 'connection,' 'password,' and 'publicKey' are required, but 'name,' 'location,' and 'ipv6' are optional. They are recommended for identifying the target of the connection.
+//This will exit without making changes to 'conf' if any of the required arguments are not provided.
+func ConnectTo(conf *cjdngo.Conf, connection string, password string, publicKey string, name string, location string, ipv6 string) {
+	if connection == "" || password == "" || publicKey == "" {
+		return //if the required fields are not filled out, exit
+	}
+	
+	conn := &cjdngo.Connection{name, location, ipv6, password, publicKey}
+	//creating the connnection
+	conf.Interfaces.UDPInterfaceBlock[connection] = conn
+	//add the new entry
+}
+/*
+func ConnectToJSON(conf *cjdngo.Conf, connections map[string]conf.Connection) {
+	if len(connections) == nil {
+		return
+	}
+	conf.Inter*/
