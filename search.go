c177e83a076988273109c1a4ae6725212af966df
SashaCrofter <sasha@crofter.org>
2012-10-02 23:25:14 -0400

    added SearchAuth

--- search.go
+++ search.go
@@ -7,7 +7,7 @@ import (
 
 //SearchAuth looks for 'term' in the non-essential elements of cjdngo.AuthPass, 'Name,' 'Location,' and 'IPv6.'
 //Returns the indexes of every AuthPass with at least one partial match in any of the above fields.
-func SearchAuth(conf cjdngo.Conf, term string) []int {
+func SearchAuth(conf *cjdngo.Conf, term string) []int {
 	matches := make([]int, 0, len(conf.AuthorizedPasswords))
 
 	for i := 0; i < len(conf.AuthorizedPasswords); i++ {
