5c26dd060a69732dcbe499d991b9b88b1ceb80a9
Sasha Crofter <sasha@crofter.org>
2012-11-16 19:35:52 -0500

    added flags and UI syntax
    The basic syntax is "gocjdroute subcommand arguments" where subcommand
    is something like "auth" "conn" or "remove", though only "auth" is
    implemented currently. The "arguments" area is parsed based on the
    command, or replaced by a simple default value.
    
    This also adds a framework for adding and implementing further commands
    and flags.

--- gocjdroute.go
+++ gocjdroute.go
@@ -1,26 +1,70 @@
 package main
 
 import (
+	"flag"
 	"github.com/SashaCrofter/cjdngo"
 	"log"
+	"os"
+	"strconv"
+	"strings"
 )
 
 const (
-	Version  = "1.0.1"
-	ConfPath = "./example.conf"
-	//LogOutput = ioutil.Discard
+	Version = "1.0.1"
+
+	authCmd   = "auth"
+	connCmd   = "conn"
+	lsAuthCmd = "lsa"
+	lsConnCmd = "lsc"
+	rmCmd     = "rm"
 )
 
 var (
 	Conf *cjdngo.Conf
+
+	File string //The file (set by flag) to edit or view.
+
+	cmd      string //The action to perform, (auth, conn, lsa, lsc, rm)
+	argument string //The argument following cmd, a search term, index, or name
 )
 
+func init() {
+	const (
+		defaultFile = "./example.conf"
+		usageFile   = "the cjdroute configuration file to edit or view"
+	)
+	flag.StringVar(&File, "file", defaultFile, usageFile)
+	flag.StringVar(&File, "f", defaultFile, usageFile+" (shorthand)")
+}
+
+func usage() {
+	println("GoCjdroute version", Version)
+	println("usage:", os.Args[0], ""+authCmd+"|"+connCmd+"|"+lsAuthCmd+"|"+lsConnCmd+"|"+rmCmd+"", "[index, identifier, or search term]")
+	println("Please use", os.Args[0], "--help", "for a list of flags.")
+}
+
 func main() {
-	Conf, err := cjdngo.ReadConf(ConfPath)
+	//Define the flags, and parse them.
+	flag.Parse()
+
+	cmd = strings.ToLower(flag.Arg(0))
+	argument = flag.Arg(1)
+
+	Conf, err := cjdngo.ReadConf(File)
 	if err != nil {
 		log.Fatal(err)
 	}
 	//log.SetOutput(ioutil.Discard)
 
-	Authorize(Conf, -1, nil)
+	//Perform an appropriate action, based on the subcommand.
+	switch cmd {
+	case authCmd:
+		index, err := strconv.Atoi(argument)
+		if err != nil {
+			index = -1
+		}
+		Authorize(Conf, index, nil)
+	default:
+		usage()
+	}
 }
