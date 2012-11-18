alexander-bauer/gocjdroute

commit 6ff7e12cc2e0c897c75d647ce338cf9fd5fc4b71

fixed lsa index displays
Previously, there was a bug involving each item shown by lsa being its
index in the display queue, rather than its index in the config file.
This has now been fixed.

closes #11

authconn.go
@@ -149,11 +149,13 @@ func Connect(conf *cjdngo.Conf, connDetails string, jsonArg []byte) {
 //ListAuthorization is meant to display authorization blocks based on a search term. All authoriziation blocks are displayed if the term is omitted. Otherwise, only authorization blocks which have a name, location, IPv6, or password which partially matches the term are displayed.
 func ListAuthorization(conf *cjdngo.Conf, term string) {
 	display := make([]cjdngo.AuthPass, 0)
+	indexes := make([]int, 0)
 
 	for i := range conf.AuthorizedPasswords {
 		pw := conf.AuthorizedPasswords[i]
 		if strings.Contains(pw.Name, term) || strings.Contains(pw.Location, term) || strings.Contains(pw.IPv6, term) || strings.Contains(pw.Password, term) {
 			display = append(display, pw)
+			indexes = append(indexes, i)
 		}
 	}
 	if len(display) == 0 {
@@ -170,7 +172,7 @@ func ListAuthorization(conf *cjdngo.Conf, term string) {
 	for i := 0; i < len(display); i++ {
 		//Add a comment to every AuthPass element
 		//marking it with its index.
-		b = bytes.Replace(b, []byte("    {\n"), []byte("    { // "+strconv.Itoa(i)+"\n"), 1)
+		b = bytes.Replace(b, []byte("    {\n"), []byte("    { // "+strconv.Itoa(indexes[i])+"\n"), 1)
 	}
 	fmt.Println(string(b))
 }

commit 823fa224e8d547a3939e0542b453ff60999ee759

minor syntax error involving previous change
commit 6ff7e12cc2e0c897c75d647ce338cf9fd5fc4b71 introduced a syntactical
error, which this commit fixes.

authconn.go
@@ -182,7 +182,7 @@ func ListConnection(conf *cjdngo.Conf, term string) {
 	display := make(map[string]cjdngo.Connection)
 
 	for k, v := range conf.Interfaces.UDPInterface.ConnectTo {
-		if strings.Contains(k, term), strings.Contains(v.Name, term) || strings.Contains(v.Location, term) || strings.Contains(v.IPv6, term) || strings.Contains(v.Password, term) || strings.Contains(v.PublicKey, term) {
+		if strings.Contains(k, term) || strings.Contains(v.Name, term) || strings.Contains(v.Location, term) || strings.Contains(v.IPv6, term) || strings.Contains(v.Password, term) || strings.Contains(v.PublicKey, term) {
 			display[k] = v
 		}
 	}